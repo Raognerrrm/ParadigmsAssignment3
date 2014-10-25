47abbc93f5ace2e8782ae064e6e3d3138a88b14a
Ben Bowring <ben.bowring14@gmail.com>
2014-10-25 15:28:04 +1100

    Finished unreachable code removal
    It is done

--- Optimisanaitanu/Unreachable.hs
+++ Optimisanaitanu/Unreachable.hs
@@ -62,37 +62,52 @@ getBlocksBlocks _ = []
 getBlocksFunction :: IFunction -> [(Node,[Node])]
 getBlocksFunction (IFunction name _ b) = getBlocksBlocks b
 
-removeBlocks :: [Node] -> IFunction -> [Node]
-removeBlocks n _ = n
-
 getNew :: [Node] -> [Node] -> Node
-getNew (a:rest) stuff = if (fst a) in stuff then getNew rest stuff
-    else fst a
+getNew (a:rest) stuff = if (isin a stuff) then getNew rest stuff
+    else a
 getNew _ _ = -1
 
 getChildren :: [(Node,[Node])] -> Node -> [Node]
-getChildren (a:rest) n = if (fst a) == n then last a
-    else getChildren rest
+getChildren (a:rest) n = if (fst a) == n then snd a
+    else getChildren rest n
 getChildren _ _ = []
 
-bfs :: [(Node,[Node])] -> Node -> [Node] -> [Node] -> [Node]
-bfs _ -1 _ stuff = stuff
-bfs graph n searched current = bfs graph (getNew (searched ++ [n]) (current ++ (getChildren graph n))) (searched ++ [n]) (current ++ (getChildren graph n))
-bfs _ = []
+isin :: Node -> [Node] -> Bool
+isin n (a:rest) = if (n==a) then True
+  else isin n rest
+isin _ _ = False
 
-getBlocks :: [IFunction] -> [[Node]]
-getBlocks (f:rest) = [removeBlocks (bfs (getBlocksFunction f) 0 [0])  f]-- ++ (getBlocks rest)
+removeDups :: [Node] -> [Node]
+removeDups (a:rest) = if (isin a rest) then removeDups rest
+  else [a] ++ (removeDups rest)
+removeDups _ = []
+
+bfs :: [(Node,[Node])] -> Node -> [Node] -> [Node] -> [Node]
+bfs graph n searched current = if (n == -1) then current
+  else bfs graph (getNew (searched ++ [n]) (removeDups (current ++ (getChildren graph n))))
+  (searched ++ [n]) (removeDups(current ++ (getChildren graph n)))
+
+removeBlocksBlocks :: [IBlock] -> [Node] -> [IBlock]
+removeBlocksBlocks ((IBlock num i):rest) n = if (isin num n) then [(IBlock num i)] ++ (removeBlocksBlocks rest n)
+  else removeBlocksBlocks rest n
+removeBlocksBlocks _ _ = []
+
+removeBlocks :: [Node] -> IFunction -> IFunction
+removeBlocks n (IFunction name args blocks) = (IFunction name args (removeBlocksBlocks blocks n))
+
+getBlocks :: [IFunction] -> [IFunction]
+getBlocks (f:rest) = [removeBlocks (bfs (getBlocksFunction f) 0 [] [0]) f] ++ (getBlocks rest)
 getBlocks _ = []
 
-removeUnreachable :: IProgram -> [[Node]]
-removeUnreachable (IProgram funcs) = getBlocks funcs
+removeUnreachable :: IProgram -> IProgram
+removeUnreachable (IProgram funcs) = (IProgram (getBlocks funcs))
 
 -- Removes unreachable code  
-unreachableFile :: FilePath -> IO [[Node]]
+unreachableFile :: FilePath -> IO IProgram
 unreachableFile file = do
    parsed_prog <- parseFile file
    return (removeUnreachable parsed_prog)
    
 printUnreachableFile file = do
    removed <- unreachableFile file
-   putStr ((show removed)++"\n")
+   putStrLn ((showIProg removed)++"\n")
