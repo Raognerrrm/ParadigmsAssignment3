65767bf11d139a4670ef6bd39a1d445d803e5e2c
Ben Bowring <ben.bowring14@gmail.com>
2014-10-28 10:37:21 +1100

    Added reverse graph
    And some comments

--- Optimisanaitanu/FlowGraph.hs
+++ Optimisanaitanu/FlowGraph.hs
@@ -3,6 +3,14 @@ import IParser
 
 type Node = Int
 
+--Random helper functions
+isin :: Node -> [Node] -> Bool
+isin n (a:rest) = if (n==a) then True
+  else isin n rest
+isin _ _ = False
+
+
+--Get Block instructions
 getBlocksInstruction :: IInstruction -> [Node]
 getBlocksInstruction (Ibr r b1 b2) = [b1,b2]
 getBlocksInstruction _ = []
@@ -28,13 +36,29 @@ getBlocksFuncs _ = []
 getBlocksProg :: IProgram -> [[(Node,[Node])]]
 getBlocksProg (IProgram funcs) = getBlocksFuncs funcs
 
-reverseGraph :: [(Node,[Node])] -> [(Node,[Node])]
+
+--Reverse graph functions
+reverseNode2 :: Node -> [(Node,[Node])] -> [Node]
+reverseNode2 n (a:rest) = if (isin n (snd a)) then [fst a] ++ (reverseNode2 n rest)
+    else reverseNode2 n rest
+reverseNode2 _ _ = []
+
+reverseNode :: (Node,[Node]) -> [(Node,[Node])] -> (Node,[Node])
+reverseNode n dat = (fst n,reverseNode2 (fst n) dat)
+
+reverseGraphSingle :: [(Node,[Node])] -> [(Node,[Node])] -> [(Node,[Node])]
+reverseGraphSingle (a:rest) dat = [reverseNode a dat] ++ (reverseGraphSingle rest dat)
+reverseGraphSingle _ _ = []
+
+reverseGraph :: [[(Node,[Node])]] -> [[(Node,[Node])]]
+reverseGraph (a:rest) = [reverseGraphSingle a a] ++ (reverseGraph rest)
 reverseGraph _ = []
 
+--Code to run the thing
 flowFile :: FilePath -> IO [[(Node,[Node])]]
 flowFile file = do
    parsed_prog <- parseFile file
-   return (getBlocksProg parsed_prog)
+   return (reverseGraph (getBlocksProg parsed_prog))
    
 printFlowFile file = do
    removed <- flowFile file
