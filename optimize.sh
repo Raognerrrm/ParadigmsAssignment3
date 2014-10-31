da06d16c9a098ceb5e7b7264e89bded302b084e4
Ant <antonyh3@hotmail.com>
2014-11-01 00:11:00 +1100

    House cleaning

--- optimize.sh
+++ optimize.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 cd Optimisanaitanu
-fileIn=$1 #unoptimised code
-
-fileOut=$2 #optimised code
-
-opt=$3 #what optimiser(s) to run
+fileIn=$1
+fileOut=$2
+opt=$3
 case $opt in
-    1) echo "Doing nothing" ;; #do nothing
-	2) echo printUnreachableFile \"../$fileIn\" | ghci Unreachable.hs -v0 > ../$fileOut ;; #do unreachable
-	3) echo deadcode_file \"../$fileIn\" | ghci Deadcode.hs -v0 > ../$fileOut ;; #do deadcode
-	4) echo printRedundantFile \"../$fileIn\" | ghci Redundant.hs -v0 > ../$fileOut ;;#do redundant
+    1) echo "Doing nothing" ;;
+	2) echo printUnreachableFile \"../$fileIn\" | ghci Unreachable.hs -v0 > ../$fileOut ;;
+	3) echo deadcode_file \"../$fileIn\" | ghci Deadcode.hs -v0 > ../$fileOut ;;
+	4) echo printRedundantFile \"../$fileIn\" | ghci Redundant.hs -v0 > ../$fileOut ;;
 	5) echo printUnreachableFile \"../$fileIn\" | ghci Unreachable.hs -v0 > ../temp.intermediate
 	echo deadcode_file \"../temp.intermediate\" | ghci Deadcode.hs -v0 > ../$fileOut
 	rm ../temp.intermediate ;;
@@ -26,13 +24,4 @@ case $opt in
 	rm ../temp.intermediate2 ;;
 	*) echo "Invalid Option" ;;
 esac
-
-#1 - nothing
-#2 - unreachable
-#3 - deadcode
-#4 - redundant
-#5 - unreachable deadcode
-#6 - unreachable redundant
-#7 - deadcode redundant
-#8 - unreachable deadcode redundant
 cd ../
