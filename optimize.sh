6c912940f26fe8d67f8280a0349193a445f59c23
Ant <antonyh3@hotmail.com>
2014-11-01 00:00:08 +1100

    Fixed redundant

--- optimize.sh
+++ optimize.sh
@@ -9,18 +9,18 @@ case $opt in
     1) echo "Doing nothing" ;; #do nothing
 	2) echo printUnreachableFile \"../$fileIn\" | ghci Unreachable.hs -v0 > ../$fileOut ;; #do unreachable
 	3) echo deadcode_file \"../$fileIn\" | ghci Deadcode.hs -v0 > ../$fileOut ;; #do deadcode
-	4) echo printRedundantFile \"../$fileIn\" | ghci Deadcode.hs -v0 > ../$fileOut ;;#do redundant
+	4) echo printRedundantFile \"../$fileIn\" | ghci Redundant.hs -v0 > ../$fileOut ;;#do redundant
 	5) echo printUnreachableFile \"../$fileIn\" | ghci Unreachable.hs -v0 > ../temp.intermediate
 	echo deadcode_file \"../temp.intermediate\" | ghci Deadcode.hs -v0 > ../$fileOut
 	rm ../temp.intermediate ;;
 	6) echo printUnreachableFile \"../$fileIn\" | ghci Unreachable.hs -v0 > ../temp.intermediate
-	echo printRedundantFile \"../temp.intermediate\" | ghci Deadcode.hs -v0 > ../$fileOut
+	echo printRedundantFile \"../temp.intermediate\" | ghci Redundant.hs -v0 > ../$fileOut
 	rm ../temp.intermediate ;;
 	7) echo deadcode_file \"../$fileIn\" | ghci Deadcode.hs -v0 > ../temp.intermediate
-	echo printRedundantFile \"../temp.intermediate\" | ghci Deadcode.hs -v0 > ../$fileOut
+	echo printRedundantFile \"../temp.intermediate\" | ghci Redundant.hs -v0 > ../$fileOut
 	rm ../temp.intermediate ;;
 	8) echo printUnreachableFile \"../$fileIn\" | ghci Unreachable.hs -v0 > ../temp.intermediate
-	echo printRedundantFile \"../temp.intermediate\" | ghci Deadcode.hs -v0 > ../temp.intermediate2
+	echo printRedundantFile \"../temp.intermediate\" | ghci Redundant.hs -v0 > ../temp.intermediate2
 	echo deadcode_file \"../temp.intermediate2\" | ghci Deadcode.hs -v0 > ../$fileOut
 	rm ../temp.intermediate
 	rm ../temp.intermediate2 ;;
